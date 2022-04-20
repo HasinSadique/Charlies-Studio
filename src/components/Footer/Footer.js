1a99d77dd2db3e835b689b7f0e4e68bae50cc252
hasinsadique <hasinsadique7@gmail.com>
2022-04-20 15:17:18 +0600

    Fully done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className=" text-white p-20 bg-slate-800">
-      <h3 className=""> All rights reserved </h3>
+      <h3 className=""> Coptright © Hasin Sadique. All rights reserved </h3>{" "}
     </div>
   );
 };
